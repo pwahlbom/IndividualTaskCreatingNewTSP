e833166b8f208161e9b37445f5d5a64ea374924b
Philip Wahlbom <philip@wahlbom.net>
2016-07-31 16:37:16 -0500

    o'tay!

--- src/IndividualTaskCreatingNewTSP/wwwroot/_references.js
+++ src/IndividualTaskCreatingNewTSP/wwwroot/_references.js
@@ -1,5 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="app.js" />
+/// <reference path="file.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
